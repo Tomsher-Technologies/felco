0785a285d92c8c66384a0ca8450a7c0d908054e4
muhsiltomsher <muhsil@tomsher.com>
2025-08-13 09:55:25 +0400

    ASA

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -10,16 +10,25 @@
     <link rel="canonical" href="{{ request()->url() }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}" />
 
+    <!-- Preconnect to external resources -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Mona+Sans:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
 
+    <!-- FontAwesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon" />
 
+    <!-- Google reCAPTCHA -->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
+    <!-- Additional CSS and JS assets -->
+    <link rel="stylesheet" href="{{ asset('public/build/assets/app-DNDxSBlH.css') }}" />
+    <link rel="stylesheet" href="{{ asset('public/build/assets/app-B9igcZhZ.css') }}" />
+    <script src="{{ asset('public/build/assets/app-Cggqv-8F.js') }}" defer></script>
+
+    <!-- Vite Assets -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @yield('header')
@@ -36,8 +45,4 @@
     </div>
   </div>
 </body>
-
-
-
-
 </html>
