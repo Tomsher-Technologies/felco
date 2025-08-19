d6b1552faa7c50102c79c8e8f77f350220932e94
s7xdd <kmshamil123@gmail.com>
2025-08-19 16:31:11 -0400

    add

--- resources/views/frontend/category_details.blade.php
+++ resources/views/frontend/category_details.blade.php
@@ -240,7 +240,7 @@
                             <a href="{{ route('products.category', ['category_slug' => $cat->slug]) }}"
                                 class="group relative block h-[30rem] w-80 flex-shrink-0 overflow-hidden rounded-lg shadow-lg md:w-96">
 
-                                <img src="{{ uploaded_asset($cat->image_2) }}"
+                                <img src="{{ $cat->image_2 ? uploaded_asset($cat->image_2) : uploaded_asset($cat->image) }}"
                                     alt="{{ $cat->getTranslation('name', $lang) }}"
                                     class="absolute inset-0 h-full w-full object-cover object-top transition-transform duration-500 ease-in-out group-hover:scale-105">
 
@@ -315,7 +315,7 @@
                             <a href="{{ route('products.category', ['category_slug' => $cat->slug]) }}"
                                 class="group relative block h-[30rem] w-80 flex-shrink-0 overflow-hidden rounded-lg shadow-lg md:w-96">
 
-                                <img src="{{ uploaded_asset($cat->image_2) }}"
+                                <img src="{{ $cat->image_2 ? uploaded_asset($cat->image_2) : uploaded_asset($cat->image) }}"
                                     alt="{{ $cat->getTranslation('name', $lang) }}"
                                     class="absolute inset-0 h-full w-full object-cover object-top transition-transform duration-500 ease-in-out group-hover:scale-105">
 
@@ -338,6 +338,8 @@
             </x-container>
         </section>
     @endif
+
+    
 
 @endsection
 
